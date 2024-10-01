1bb7b9326fee5d3ce5540bd11f85c676fc52917e
EmperorRaccoonthe1st <91690761+EmperorRaccoonthe1st@users.noreply.github.com>
2024-10-01 15:56:59 -0400

    Add files via upload

---
+++ meeting2/fprint.py
@@ -0,0 +1,30 @@
+# it is common in langueges to have an "fprint" function
+# in python you just put the f in the print statement
+# in an fprint, you can insert varibles in {} straight into the string
+# much easier
+
+
+var1 = 5
+var2 = 6777
+var3 = 5.378
+varList = ["apple", "bannana", "cookie"]
+
+
+print(var1)
+
+print("###")
+
+print(var2)
+
+print("###")
+
+print("Hellow " + str(var2))
+print(f"Hellow {var2}")
+
+print("###")
+
+print(f"This is a int: {var1} and this is a float: {var3}")
+print(f"And this a list! {varList}")
+ 
+# see how the varibles are actually inside the quotes
+
---
+++ meeting2/whileLoops.py
@@ -0,0 +1,23 @@
+# While loops run "while" something is true
+# This can be any value of a variable in a conditional
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
