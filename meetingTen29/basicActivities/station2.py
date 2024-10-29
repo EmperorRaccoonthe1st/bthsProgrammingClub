35c3da64a4c7c85e037fa413f547d702a1b32f2c
EmperorRaccoonthe1st <owenmlopez07@gmail.com>
2024-10-29 15:36:42 -0400

    renamed:    meeting2/var -> meeting2/var.py 	new file:   meetingTen29/basicActivities/station1.py 	new file:   meetingTen29/basicActivities/station2.py 	new file:   meetingTen29/basicActivities/station3.py 	new file:   meetingTen29/basicActivities/station4.py 	new file:   meetingTen29/basicActivities/station6.py 	modified:   meetingTen29/exampleFunctions.py

---
+++ meetingTen29/basicActivities/station2.py
@@ -0,0 +1,30 @@
+# Write a Python code to iterate the first 10 numbers, 
+# and in each iteration, print the sum of the current and previous number.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
