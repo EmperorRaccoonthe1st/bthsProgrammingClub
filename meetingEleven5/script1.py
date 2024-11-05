2f9022006274e2827376bf90e3d82aebe29a90f9
EmperorRaccoonthe1st <owenmlopez07@gmail.com>
2024-11-05 16:54:48 -0500

    modified:   meetingEleven5/script1.py 	modified:   meetingEleven5/script2.py 	modified:   meetingEleven5/script3.py

--- meetingEleven5/script1.py
+++ meetingEleven5/script1.py
@@ -1,8 +1,15 @@
-print("Hello, world!")
+print("#")
 
+wordList = list("Hello world I'm Owen")
 
+statement = ""
 
+var = 0
 
+while var < 20:
+    statement += wordList[var]
+    var += 1
+print(statement)
 
 
 
--- meetingEleven5/script2.py
+++ meetingEleven5/script2.py
@@ -1,10 +1,48 @@
 # Rewrite it with a for and while loop
-print("1")
-print("2")
-print("3")
-print("4")
-print("5")
+num = 0
+for i in range(6):
+    print(num)
+    num = num + 1
 
+num = 0
+for i in range(6):
+    print(num)
+    num -= 1
+
+def printTimes(num:int):
+    for i in range(num):
+        print(i)
+
+printTimes(10)
+
+var = 1
+
+while var < 6:
+    print(var)
+    var += 1
+
+var = 1
+while var <= 5:
+    print(var)
+    var += 1
+
+var = 1
+while var != 6:
+    print(var)
+    var += 1
+
+var = 0
+while var != 5:
+    print(var + 1)
+    var += 1
+
+var = 1
+while True:
+    print(var)
+    if var == 5:
+        break
+    else:
+        var += 1
 
 
 
