d115477ce35276c76b0ecc9270b3089bfd6f8f2f
EmperorRaccoonthe1st <owenmlopez07@gmail.com>
2024-11-12 16:59:37 -0500

    modified:   meetingTen29/basicActivities/station3.py

--- meetingTen29/basicActivities/station3.py
+++ meetingTen29/basicActivities/station3.py
@@ -1,8 +1,24 @@
-# Write a Python code to accept a string from the user and display characters present at an even index number.
+print("#")
+# Write a Python code to accept a string from the user
+#  and display characters present at an even index number.
 
+# hello
+# 01234
 
+# Get User Input
+# run a for loop over it
+# check even
+# add it to a list of the letters that are even
+# print that list
 
+result = ""
+li = list(input())
 
+for i in range(len(li)):
+    if i % 2 == 0:
+        result += li[i] + " "
+else:
+    print(result)
 
 
 
