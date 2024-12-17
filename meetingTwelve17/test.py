d0f7de081f4180e3e5992141d2acca7596bfe517
EmperorRaccoonthe1st <owenmlopez07@gmail.com>
2024-12-17 16:21:21 -0500

    new file:   OwenXmasLetter.txt 	modified:   meetingTwelve17/test.py

--- meetingTwelve17/test.py
+++ meetingTwelve17/test.py
@@ -1,13 +1,26 @@
 print("###")
 name: str = input("What is your name?")
-fileName: str = name + "XmasLetter.txt"
+mainFileName: str = name + "XmasLetter.txt"
 
-with open(fileName, "w") as file:
-    file.write("Hey!")
-    file.write(" Whats going on!.\n")
-    file.write("Merry Xmas!!!!")
-    file.write("\nDoes this work?")
+def main():
+    startLetter(mainFileName)
 
+def startLetter(fileName:str):
+    with open(fileName, "w") as file:
+        file.write("Hey!")
+        file.write(" Whats going on!.\n")
+        file.write("Merry Xmas!!!!")
+        file.write("\nDoes this work?")
+
+def appendSmallTalk(fileName:str):
+    pass
+    # open file w/a
+    # write into it the "small talk"
+    # mad libs and decision trees
+
+def appendBrag(fileName:str):
+    with open(fileName, "a") as file:
+        pass
 
 
 
